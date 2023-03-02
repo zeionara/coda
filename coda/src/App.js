ff2b7ac8c0dde2a8751e65f46f7f150d6634de86
dima <zeionara@niuitmo.ru>
2023-03-02 21:36:24 +0000

    Switched to custom style

--- coda/src/App.js
+++ coda/src/App.js
@@ -1,36 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import './styles/app.sass';
 
 import { Command, Argument } from './components/terminal'
 
-function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>
-}
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Welcome name="zeio"/>
+    <div className="app">
+        <h1>Here are your commands</h1>
         <p>
             <Command name="coda"/>
             <Argument name="describe" description="describe command features" theme="blue"/>
             <Argument name="bar" description="describe command features" theme="red"/>
             <Argument name="fox" description="describe command features" theme="red"/>
         </p>
-      </header>
     </div>
   );
 }
