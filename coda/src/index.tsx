070acdc6765ee4111896d299f5f0fd063c868654
Dmitrii <zeionara@gmail.com>
2023-03-09 23:35:35 +0300

    Published the first working release

--- coda/src/index.tsx
+++ coda/src/index.tsx
@@ -1,22 +1,27 @@
-import * as React from 'react';
-import { createRoot } from 'react-dom/client';
+// import * as React from 'react';
+// import { createRoot } from 'react-dom/client';
+// import './styles/index.sass';
+// import App from './App';
+// // import reportWebVitals from './reportWebVitals';
+// 
+// // const root = ReactDOM.createRoot(document.getElementById('root'));
+// 
+// createRoot(document.getElementById('root') as HTMLElement).render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>
+// )
+// 
+// // root.render(
+// //
+// // root.render(app)
+// 
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// // reportWebVitals();
+
 import './styles/index.sass';
-import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
-// root.render(
-//
-// root.render(app)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+import './styles/app.sass';
 
 export * from './components'
